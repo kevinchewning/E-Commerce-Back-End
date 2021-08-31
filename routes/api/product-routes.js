d1e2b0a05a913253e2fc41c20343d459521fce74
kevinchewning <klcbusiness@hotmail.com>
2021-08-31 18:12:00 -0400

    finished writing routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -56,77 +56,75 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Basketball",
+      "price": "200.00",
+      "stock": "3",
+      "category_id": "5",
+      "tagIds": "[1, 2, 3, 4]"
     }
   */
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
+  try {
+    const dbProductData = await Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    
+    const tagIdArray = JSON.parse(req.body.tagIds)
+    if (tagIdArray) {
+      const productTagIdArr = tagIdArray.map(tagId => {
+        return {
+          product_id: dbProductData.id,
+          tag_id: tagId,
+        };
+      });
+      ProductTag.bulkCreate(productTagIdArr);
+    }
+
+    res.status(200).json(dbProductData)
+    
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
 });
 
 // update product
-router.put('/:id', (req, res) => {
-  // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      // find all associated tags from ProductTag
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const updateProductData = await Product.update(req.body,
+      {
+        where: {
+          id: req.params.id
+        }
+      })
+
+    const destroyTags = await ProductTag.destroy({ where: { product_id: req.params.id}})
+
+    Promise.all([updateProductData, destroyTags]).then(() => {
+      if (req.body.tagIds) {
+        const tagIdArray = JSON.parse(req.body.tagIds)
+        const productTagIdArr = tagIdArray.map(tagId => {
           return {
             product_id: req.params.id,
-            tag_id,
+            tag_id: tagId,
           };
         });
-      // figure out which ones to remove
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
+        ProductTag.bulkCreate(productTagIdArr);
+      }
+      
+      res.status(200).json('Update Successful')
+    }).catch((err)=>{console.log(err)})
 
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
-    });
+    
+    } catch (err) {
+      console.log(err)
+      res.status(500).json.apply(err)
+    }
 });
 
 router.delete('/:id', async (req, res) => {
